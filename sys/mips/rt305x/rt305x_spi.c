2a56c9b2afbaa4a4541415ddc0000c12609998c5
Hiroki Mori <yamori813@yahoo.co.jp>
2015-12-25 11:31:32 +0900

    checkout master

--- sys/mips/rt305x/rt305x_spi.c
+++ sys/mips/rt305x/rt305x_spi.c
@@ -1,6 +1,7 @@
 /*-
  * Copyright (c) 2009, Oleksandr Tymoshenko <gonzo@FreeBSD.org>
  * Copyright (c) 2011, Aleksandr Rybalko <ray@FreeBSD.org>
+ * Copyright (c) 2013, Alexander A. Mityaev <sansan@adm.ua>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,27 +32,32 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/systm.h>
-
 #include <sys/bus.h>
-#include <sys/interrupt.h>
-#include <sys/malloc.h>
+
 #include <sys/kernel.h>
 #include <sys/module.h>
 #include <sys/rman.h>
-
-#include <vm/vm.h>
-#include <vm/pmap.h>
-#include <vm/vm_extern.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
 
 #include <machine/bus.h>
 #include <machine/cpu.h>
-#include <machine/pmap.h>
+//#include <machine/pmap.h>
 
 #include <dev/spibus/spi.h>
 #include <dev/spibus/spibusvar.h>
 #include "spibus_if.h"
 
+#include "opt_platform.h"
+
+#ifdef FDT
+#include <dev/ofw/openfirm.h>
+#include <dev/ofw/ofw_bus.h>
+#include <dev/ofw/ofw_bus_subr.h>
+#endif
+
 #include <mips/rt305x/rt305xreg.h>
+#include <dev/flash/mx25lreg.h>
 
 #undef RT305X_SPI_DEBUG
 #ifdef RT305X_SPI_DEBUG
@@ -88,11 +94,17 @@ static void	rt305x_spi_chip_activate(struct rt305x_spi_softc *);
 static void	rt305x_spi_chip_deactivate(struct rt305x_spi_softc *);
 static uint8_t	rt305x_spi_txrx(struct rt305x_spi_softc *, uint8_t *, int);
 static int	rt305x_spi_transfer(device_t, device_t, struct spi_command *);
-
+#ifdef FDT
+static phandle_t rt305x_spi_get_node(device_t, device_t);
+#endif
 
 static int
 rt305x_spi_probe(device_t dev)
 {
+#ifdef FDT
+        if (!ofw_bus_is_compatible(dev, "ralink,rt305x-spi"))
+            return(ENXIO);
+#endif
 	device_set_desc(dev, "RT305X SPI");
 	return (0);
 }
@@ -118,7 +130,7 @@ rt305x_spi_attach(device_t dev)
 	}
 
 	SPI_WRITE(sc, RT305X_SPICFG, MSBFIRST | SPICLKPOL | TX_ON_CLK_FALL |
-	    SPI_CLK_DIV4); /* XXX: make it configurable */
+	    SPI_CLK_DIV8); /* XXX: make it configurable */
 	    /*
 	     * W25Q64CV max 104MHz, bus 120-192 MHz, so divide by 2.
 	     * Update: divide by 4, DEV2 to fast for flash.
@@ -144,7 +156,8 @@ rt305x_spi_detach(device_t dev)
 static void
 rt305x_spi_chip_activate(struct rt305x_spi_softc *sc)
 {
-
+//        printf("%s\n", __func__);
+        rt305x_spi_wait(sc);
 	/*
 	 * Put all CSx to low
 	 */
@@ -154,7 +167,8 @@ rt305x_spi_chip_activate(struct rt305x_spi_softc *sc)
 static void
 rt305x_spi_chip_deactivate(struct rt305x_spi_softc *sc)
 {
-
+//        printf("%s\n", __func__);
+        rt305x_spi_wait(sc);
 	/*
 	 * Put all CSx to high
 	 */
@@ -188,30 +202,15 @@ rt305x_spi_txrx(struct rt305x_spi_softc *sc, uint8_t *data, int write)
 	if (write == RT305X_SPI_WRITE) {
 		SPI_WRITE(sc, RT305X_SPIDATA, *data);
 		SPI_SET_BITS(sc, RT305X_SPICTL, START_WRITE);
+		//printf("%s(W:%d)\n", __func__, *data);
 	} else {/* RT305X_SPI_READ */
 		SPI_SET_BITS(sc, RT305X_SPICTL, START_READ);
 		if (rt305x_spi_wait(sc))
 			return (EBUSY);
 
 		*data = SPI_READ(sc, RT305X_SPIDATA) & 0xff;
+		//printf("%s(R:%d)\n", __func__, *data);
 	}
-	return (0);
-}
-
-static int
-rt305x_spi_get_block(device_t dev, device_t child, off_t offset, caddr_t data,
-    off_t count)
-{
-	struct rt305x_spi_softc *sc;
-
-	sc = device_get_softc(dev);
-
-	if ((offset + count) > rman_get_size(sc->sc_mem_res))
-		return (EINVAL);
-
-	bus_read_region_4(sc->sc_mem_res, offset, (uint32_t *)data,
-	    (bus_size_t)(count/4));
-
 	return (0);
 }
 
@@ -219,7 +218,7 @@ static int
 rt305x_spi_transfer(device_t dev, device_t child, struct spi_command *cmd)
 {
 	struct rt305x_spi_softc *sc;
-	uint8_t *buf, byte;
+	uint8_t *buf, byte, *tx_buf;
 	struct spibus_ivar *devi = SPIBUS_IVAR(child);
 	int i, sz, error = 0, write = 0;
 
@@ -228,36 +227,69 @@ rt305x_spi_transfer(device_t dev, device_t child, struct spi_command *cmd)
 	if (devi->cs != 0)
 		/* Only 1 CS */
 		return (ENXIO);
-	if ((cmd->tx_cmd_sz > 0) && (cmd->rx_cmd_sz > 0))
-		/* RT305x don't support duplex SPI */
-		return (EIO);
-	if ((cmd->tx_data_sz > 0) && (cmd->rx_data_sz > 0))
-		/* RT305x don't support duplex SPI */
-		return (EIO);
 
+        /* There is always a command to transfer. */
+        tx_buf = (uint8_t *)(cmd->tx_cmd);
+        
+        /* Perform some fixup because RT305X dont support duplex SPI */
+        switch(tx_buf[0]) {
+                case CMD_READ_IDENT:
+                        cmd->tx_cmd_sz = 1;
+                        cmd->rx_cmd_sz = 3;
+                        break;
+                case CMD_WRITE_ENABLE:
+                case CMD_WRITE_DISABLE:
+                        cmd->tx_cmd_sz = 1;
+                        cmd->rx_cmd_sz = 0;
+                        break;
+                case CMD_READ_STATUS:
+                        cmd->tx_cmd_sz = 1;
+                        cmd->rx_cmd_sz = 1;
+                        break;
+                case CMD_READ:
+                        cmd->tx_cmd_sz = 4;
+                case CMD_FAST_READ:
+                        cmd->tx_cmd_sz = 5;
+                        cmd->rx_cmd_sz = cmd->tx_data_sz = 0;
+                        break;
+                case CMD_SECTOR_ERASE:
+                        cmd->tx_cmd_sz = 4;
+                        cmd->rx_cmd_sz = cmd->tx_data_sz = 0;
+                        break;
+                case CMD_PAGE_PROGRAM:
+                        cmd->tx_cmd_sz = 4;
+                        cmd->rx_cmd_sz = cmd->rx_data_sz = 0;
+                        break;
+        }      
+        
 	rt305x_spi_chip_activate(sc);
 
-	/*
-	 * Transfer/Receive command
-	 */
 	if (cmd->tx_cmd_sz + cmd->rx_cmd_sz) {
-		write = (cmd->tx_cmd_sz > 0)?1:0;
-		buf = (uint8_t *)(write ? cmd->tx_cmd : cmd->rx_cmd);
-		sz = write ? cmd->tx_cmd_sz : cmd->rx_cmd_sz;
+		buf = (uint8_t *)(cmd->rx_cmd);
+		tx_buf = (uint8_t *)(cmd->tx_cmd);
+		sz = cmd->tx_cmd_sz + cmd->rx_cmd_sz;
 
 		for (i = 0; i < sz; i++) {
-			byte = buf[i];
-			error = rt305x_spi_txrx(sc, &byte,
-			    write?RT305X_SPI_WRITE:RT305X_SPI_READ);
-			if (error)
-				goto rt305x_spi_transfer_fail;
+                        if(i < cmd->tx_cmd_sz) {
+			        byte = tx_buf[i];
+        			error = rt305x_spi_txrx(sc, &byte,
+		        	    RT305X_SPI_WRITE);
+        			if (error)
+        				goto rt305x_spi_transfer_fail;
+                                continue;
+                        }
+                        error = rt305x_spi_txrx(sc, &byte,
+		            RT305X_SPI_READ);
+        		if (error)
+        			goto rt305x_spi_transfer_fail;
 			buf[i] = byte;
 		}
 	}
-
+	
 	/*
 	 * Transfer/Receive data
 	 */
+	
 	if (cmd->tx_data_sz + cmd->rx_data_sz) {
 		write = (cmd->tx_data_sz > 0)?1:0;
 		buf = (uint8_t *)(write ? cmd->tx_data : cmd->rx_data);
@@ -272,12 +304,21 @@ rt305x_spi_transfer(device_t dev, device_t child, struct spi_command *cmd)
 			buf[i] = byte;
 		}
 	}
-
 rt305x_spi_transfer_fail:
 	rt305x_spi_chip_deactivate(sc);
 
 	return (error);
 }
+
+#ifdef FDT
+static phandle_t
+rt305x_spi_get_node(device_t bus, device_t dev)
+{
+
+	/* We only have one child, the SPI bus, which needs our own node. */
+	return (ofw_bus_get_node(bus));
+}
+#endif
 
 static device_method_t rt305x_spi_methods[] = {
 	/* Device interface */
@@ -286,17 +327,24 @@ static device_method_t rt305x_spi_methods[] = {
 	DEVMETHOD(device_detach,	rt305x_spi_detach),
 
 	DEVMETHOD(spibus_transfer,	rt305x_spi_transfer),
-	DEVMETHOD(spibus_get_block,     rt305x_spi_get_block),
 
-	{0, 0}
+#ifdef FDT
+	/* ofw_bus interface */
+	DEVMETHOD(ofw_bus_get_node,	rt305x_spi_get_node),
+#endif
+
+	DEVMETHOD_END
 };
 
 static driver_t rt305x_spi_driver = {
-	"spi",
-	rt305x_spi_methods,
-	sizeof(struct rt305x_spi_softc),
+	.name = "spi",
+	.methods = rt305x_spi_methods,
+	.size = sizeof(struct rt305x_spi_softc),
 };
 
 static devclass_t rt305x_spi_devclass;
 
 DRIVER_MODULE(rt305x_spi, obio, rt305x_spi_driver, rt305x_spi_devclass, 0, 0);
+#ifdef FDT
+DRIVER_MODULE(rt305x_spi, simplebus, rt305x_spi_driver, rt305x_spi_devclass, 0, 0);
+#endif
