34e533e89b9bb14ea0244cb091b99310060c3123
Hiroki Mori <yamori813@yahoo.co.jp>
2017-03-29 18:17:13 +0900

    cfi mx patch

--- sys/dev/cfi/cfi_reg.h
+++ sys/dev/cfi/cfi_reg.h
@@ -112,8 +112,11 @@ struct cfi_qry {
 #define	CFI_BCS_ERASE_RESUME	0xd0	/* Equals CONFIRM */
 #define	CFI_BCS_CONFIRM		0xd0
 #define	CFI_BCS_BUF_PROG_SETUP	0xe8
-//#define	CFI_BCS_READ_ARRAY	0xff
+#ifdef CFI_USE_MXFLASH
 #define	CFI_BCS_READ_ARRAY	0xf0
+#else
+#define	CFI_BCS_READ_ARRAY	0xff
+#endif
 
 /* Intel commands. */
 #define	CFI_INTEL_LB		0x01	/* Lock Block */
